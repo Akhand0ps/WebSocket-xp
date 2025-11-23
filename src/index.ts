3f2a57e9f8a5bdc1042ba052632c632e91e00b42
Akhand0ps <akhandps08@gmail.com>
2025-11-23 19:38:20 +0000

    feature: room logic and improvised chat,join logic.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import {WebSocketServer,WebSocket} from "ws";
-
+import { nanoid } from 'nanoid'
 
 const wss = new WebSocketServer({port:8080});
 
-interface User {
+// interface User {
 
-    socket: WebSocket;
-    room: string;
-}
-let allSockets: User[]=[];
+//     socket: WebSocket;
+//     room: string;
+// }
+// let allSockets: User[]=[];
+
+const rooms = new Map<string,Set<WebSocket>>();
+
+
 
 wss.on("connection", (socket)=>{
 
@@ -19,14 +23,38 @@ wss.on("connection", (socket)=>{
         const parsedMsg = JSON.parse(message);
         // console.log(parsedMsg);
 
+
+        if(parsedMsg.type=='create-room'){
+            console.log('creating room....');
+            const roomId = nanoid(6);
+
+            rooms.set(roomId, new Set([socket]));
+
+            socket.send(JSON.stringify({
+                type:"room-created",
+                payload:{roomId}
+            }))
+
+            
+        }
+
         if(parsedMsg.type === 'join'){
 
             console.log('user joined room '+parsedMsg.payload.roomId);
 
-            allSockets.push({
-                socket,
-                room:parsedMsg.payload.roomId
-            })
+            // allSockets.push({
+            //     socket,
+            //     room:parsedMsg.payload.roomId
+            // })
+
+            const roomId = parsedMsg.payload.roomId;
+
+            if(!rooms.has(roomId)){
+
+                rooms.set(roomId,new Set());
+            }
+
+            rooms.get(roomId)?.add(socket);
         }
 
         if(parsedMsg.type ==='chat'){
@@ -35,29 +63,41 @@ wss.on("connection", (socket)=>{
 
             console.log('user open to chat')
 
-            let currentUserRoom = null;
+            //more industory way
 
-            for(let i=0;i<allSockets.length;i++){
+           /* const roomId = [...rooms.entries()]
+                    .find( ([_,users]) =>users.has(socket))?.[0] 
+            */
+           let roomId = null;
 
-                if(allSockets[i]?.socket == socket){
-                    currentUserRoom = allSockets[i]?.room;
-                }
+           for(const[id,users] of rooms.entries()){
+
+            if(users.has(socket)){
+                roomId = id;
+                break;
             }
+           }
 
-            //ab broadcast kr jo iss specific room se connect hai. 
+           if(!roomId) return;
 
-            for(let i=0;i<allSockets.length;i++){
+           const message = parsedMsg.payload.message;
 
-                if(allSockets[i]?.room == currentUserRoom){
-                    allSockets[i]?.socket.send(parsedMsg.payload.message);
-                }
-            }
+           for(const userSocket of rooms.get(roomId)!)
+            userSocket.send(message);
         }
 
     })
 
     socket.on("close",()=>{
 
-        
-    })
+        for(const[roomId,socketId] of rooms.entries()){
+
+            socketId.delete(socket);
+
+            if(socketId.size ==0) rooms.delete(roomId);
+        }
+    });
+
+
+
 })
